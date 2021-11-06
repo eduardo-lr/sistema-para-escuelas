b0a61c4c40cc6b36e5e73704e3758c9045f1a35d
Eduardo <eduardo_lr@ciencias.unam.mx>
2021-11-06 16:53:45 -0600

    Clase alumno declarada

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Sequence
+
+Base = declarative_base()
+
+class Alumno(Base):
+    
+    __tablename__ = 'alumno'
+    
+    id_alumno = Column(Integer, Sequence('id_alumno_sequence'), primary_key=True)   
